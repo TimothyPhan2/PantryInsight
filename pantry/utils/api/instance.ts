6c7719a10884165e91628a496590f5a2bd427e66
TimothyPhan2 <98495706+TimothyPhan2@users.noreply.github.com>
2024-08-04 20:58:32 -0700

    update

--- pantry/utils/api/instance.ts
+++ pantry/utils/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/";
 export default axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: baseURL,
     headers: {
         "Content-type": "application/json"
     
